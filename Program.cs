c9d36c4f960832923b509af0d3bbf165fa08326a
jasmine-codes <jsmlee97@gmail.com>
2025-10-23 08:46:25 +0200

    initial setup and remove boilerplate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
