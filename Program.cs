89c7424d340dbf4f5013f5269ea2d87202e9946b
jasmine-codes <jsmlee97@gmail.com>
2025-10-23 08:49:54 +0200

    create class Habit

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
+
+public class Habit
+{
+    public string Name { get; set;}
+    public bool Completed { get; set; }
+}
